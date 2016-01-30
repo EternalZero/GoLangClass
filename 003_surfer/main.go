c3be3afc27cdd283d5c4d8195ca14175553f6b15
EternalZero <jray12332@gmail.com>
2016-01-29 22:26:44 -0800

    Surfer Webpage
    create the "surfer page" and serve it using Go

---
+++ 003_surfer/main.go
@@ -0,0 +1,29 @@
+package main
+
+import(
+
+	"net/http"
+	"html/template"
+	"log"
+)
+
+func surfsup(res http.ResponseWriter, req *http.Request) {
+	tpl, err := template.ParseFiles("tpl.html")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	tpl.Execute(res,nil)
+	}
+
+
+
+
+func main() {
+
+	http.HandleFunc("/", surfsup)
+
+	http.Handle("/surfing/", http.StripPrefix("/surfing", http.FileServer(http.Dir("assets"))))
+	http.ListenAndServe(":8080", nil)
+}
+
+
